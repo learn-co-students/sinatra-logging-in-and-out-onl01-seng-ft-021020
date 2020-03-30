dd7d06b431023423cc10b918dcb2624bd39fb00f
kevinkim <kevinkim.yhk@gmail.com>
2020-03-29 18:32:32 -0700

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+    def self.current_user(params)
+        @user = User.find_by(id: params[:user_id])
+    end 
+
+
+    def self.is_logged_in?(params)   
+        !!params[:user_id]
+    end 
 end
