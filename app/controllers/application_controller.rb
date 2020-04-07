44ca225f0ab67c1a6f23fb29a5ac4db285895417
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-07 20:27:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params["username"], password: params["password"])
    
     if user
-      session[:session_id] = user.id
+      session[:user_id] = user.id
       redirect to '/account'
     else
       erb :error
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,9 @@
-require 'pry'
-class Helpers < ActiveRecord::Base
-
-  def self.current_user(session)
-    @user = User.find_by_id(session[:session_id])
+class Helpers
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
   end
 
-  def self.is_logged_in?(session)
-    !!session[:session_id]
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
   end
 end
